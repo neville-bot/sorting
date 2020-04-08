1bd83bcbe63a1995344ade71e6ac27f61bbd21c7
Alona Trekhlib <trekhleb123@Alonas-MacBook-Air.local>
2020-04-08 14:00:55 -0500

    still working on spy

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,11 @@
+function swap(firstNum, secondNum) {
+  console.log("before", firstNum, secondNum)
+  const temp = firstNum
+  firstNum = secondNum
+  secondNum = temp
+  console.log("after", firstNum, secondNum)
+}
+
 function bubbleSort(array) {
   if (array.length === 0) return array
   if (array.length === 1) return array
@@ -8,17 +16,16 @@ function bubbleSort(array) {
   //   if (a > b) return 1
   // })
 
-  const sorting = {}
-  sorting.swap = function () {
-    return "swaped"
-  }
-
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length; j++) {
       if (array[j] > array[j + 1]) {
-        let temp = array[j]
-        array[j] = array[j + 1]
-        array[j + 1] = temp
+        // let temp = array[j]
+        // array[j] = array[j + 1]
+        // array[j + 1] = temp
+        console.log("before swap called", array[j], array[j + 1])
+        swap(array[j], array[j + 1])
+
+        console.log("after swap called", array[j], array[j + 1])
       }
     }
   }
